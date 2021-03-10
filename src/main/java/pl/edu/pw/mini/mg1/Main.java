653a7b068e65a50febb86811b01185eb9e7e3627
Tomasz Herman <tomasz.herman@protonmail.com>
2021-03-10 22:25:51 +0100

    first commit

---
+++ src/main/java/pl/edu/pw/mini/mg1/Main.java
@@ -0,0 +1,23 @@
+package pl.edu.pw.mini.mg1;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.hermant.swing.WindowBuilder;
+import com.jogamp.opengl.GLContext;
+import pl.edu.pw.mini.mg1.layout.MainLayout;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FlatDarculaLaf.install();
+        MainLayout mainLayout = new MainLayout();
+        JFrame window = new WindowBuilder()
+                .setContentPane(mainLayout.getMainPane())
+                .setSize(1280, 720)
+                .setMinimumSize(320, 240)
+                .setTitle("GModeling")
+                .setExitOnClose()
+                .buildFrame();
+    }
+}
