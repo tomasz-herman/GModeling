ed0f760ea2da832a376e7d279beb5dc97bd1044e
Tomasz Herman <tomasz.herman@protonmail.com>
2021-06-22 15:40:24 +0200

    Finding intersection start

---
+++ src/main/java/pl/edu/pw/mini/mg1/numerics/IntersectionStart.java
@@ -0,0 +1,106 @@
+package pl.edu.pw.mini.mg1.numerics;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static org.joml.Math.*;
+
+public class IntersectionStart {
+    private final Function<Vector2f, Vector3f> P;
+    private final Function<Vector2f, Vector3f> Q;
+    Random random = new Random();
+
+    public IntersectionStart(Function<Vector2f, Vector3f> p, Function<Vector2f, Vector3f> q) {
+        P = p;
+        Q = q;
+    }
+
+    public IntersectionStart(BiFunction<Float, Float, Vector3f> p, BiFunction<Float, Float, Vector3f> q) {
+        P = vec -> p.apply(vec.x, vec.y);
+        Q = vec -> q.apply(vec.x, vec.y);
+    }
+
+    public Vector4f solve(Vector3f closeTo) {
+        Pair<Vector2f, Vector3f> p = Pair.of(new Vector2f(0.5f), P.apply(new Vector2f(0.5f)));
+        Pair<Vector2f, Vector3f> q = Pair.of(new Vector2f(0.5f), Q.apply(new Vector2f(0.5f)));
+        float R = 1f;
+        if(closeTo != null) {
+            List<Pair<Vector2f, Vector3f>> pTemp = new ArrayList<>();
+            List<Pair<Vector2f, Vector3f>> qTemp = new ArrayList<>();
+            for (int j = 0; j < 65536; j++) {
+                Vector2f puv = uv(new Vector2f(rand(1), rand(1)).add(p.getLeft()));
+                Vector2f quv = uv(new Vector2f(rand(1), rand(1)).add(q.getLeft()));
+                pTemp.add(Pair.of(puv, P.apply(puv)));
+                qTemp.add(Pair.of(quv, Q.apply(quv)));
+            }
+            p = pTemp.stream().min((p1, p2) -> {
+                float d1 = p1.getRight().distanceSquared(closeTo);
+                float d2 = p2.getRight().distanceSquared(closeTo);
+                return Float.compare(d1, d2);
+            }).orElseThrow();
+            q = qTemp.stream().min((p1, p2) -> {
+                float d1 = p1.getRight().distanceSquared(closeTo);
+                float d2 = p2.getRight().distanceSquared(closeTo);
+                return Float.compare(d1, d2);
+            }).orElseThrow();
+            R = 0.1f;
+        }
+        for (int i = 0; i < 32; i++) {
+            List<Pair<Vector2f, Vector3f>> pTemp = new ArrayList<>();
+            List<Pair<Vector2f, Vector3f>> qTemp = new ArrayList<>();
+            for (int j = 0; j < (closeTo == null ? 1024 : 4096); j++) {
+                Vector2f puv = uv(new Vector2f(rand(R), rand(R)).add(p.getLeft()));
+                Vector2f quv = uv(new Vector2f(rand(R), rand(R)).add(q.getLeft()));
+                pTemp.add(Pair.of(puv, P.apply(puv)));
+                qTemp.add(Pair.of(quv, Q.apply(quv)));
+            }
+            if(closeTo != null) {
+                pTemp.sort((p1, p2) -> {
+                    float d1 = p1.getRight().distanceSquared(closeTo);
+                    float d2 = p2.getRight().distanceSquared(closeTo);
+                    return Float.compare(d1, d2);
+                });
+                qTemp.sort((p1, p2) -> {
+                    float d1 = p1.getRight().distanceSquared(closeTo);
+                    float d2 = p2.getRight().distanceSquared(closeTo);
+                    return Float.compare(d1, d2);
+                });
+            }
+            pTemp = pTemp.subList(0, 1024);
+            qTemp = qTemp.subList(0, 1024);
+            float lastDist = p.getRight().distanceSquared(q.getRight());
+            float bestDist = lastDist;
+            for (Pair<Vector2f, Vector3f> qq : qTemp) {
+                for (Pair<Vector2f, Vector3f> pp : pTemp) {
+                    float nextDist = qq.getRight().distanceSquared(pp.getRight());
+                    if (nextDist < bestDist) {
+                        bestDist = nextDist;
+                        q = qq;
+                        p = pp;
+                    }
+                }
+            }
+            if(lastDist > bestDist) R = R * sqrt(bestDist / lastDist);
+            if(bestDist < 1e-12f) return new Vector4f(p.getLeft(), q.getLeft().x, q.getLeft().y);;
+        }
+        return null;
+    }
+
+    private float rand(float r) {
+        return r * (random.nextFloat() - 0.5f);
+    }
+
+    private Vector2f uv(Vector2f uv) {
+        uv.x = clamp(0, 1, uv.x);
+        uv.y = clamp(0, 1, uv.y);
+        return uv;
+    }
+}
--- src/test/java/pl/edu/pw/mini/mg1/numerics/NewtonTest.java
+++ src/test/java/pl/edu/pw/mini/mg1/numerics/NewtonTest.java
@@ -13,18 +13,19 @@ import static org.joml.Math.sin;
 class NewtonTest {
     @Test
     public void test() {
-        final BiFunction<Float, Float, Vector3f> P = (u, v) -> new Vector3f(cos(u) * sin(v), sin(u) * sin(v), cos(v));
-        final BiFunction<Float, Float, Vector3f> Q = (u, v) -> new Vector3f(cos(u) * sin(v) + 1, sin(u) * sin(v), cos(v));
+        final BiFunction<Float, Float, Vector3f> P = (u, v) -> new Vector3f(cos(u * 2 * PI) * sin(v * 2 * PI), sin(u * 2 * PI) * sin(v * 2 * PI), cos(v * 2 * PI));
+        final BiFunction<Float, Float, Vector3f> Q = (u, v) -> new Vector3f(cos(u * 2 * PI) * sin(v * 2 * PI) + 1, sin(u * 2 * PI) * sin(v * 2 * PI), cos(v * 2 * PI));
         final BiFunction<Float, Float, Vector3f> Pn = P.andThen(Vector3f::negate);
         final BiFunction<Float, Float, Vector3f> Qn = P.andThen(Vector3f::negate);
-        Vector4f x0 = new Vector4f(PI, PI * 7/6, 0, PI * 7/6);
+//        Vector4f x0 = new Vector4f(0.5f, 0.5f * 7/6, 0, 0.5f * 7/6);
+        Vector4f x0 = new IntersectionStart(P, Q).solve(null);
+//        Vector4f x0 = new IntersectionStart(P, Q).solve(P.apply(0.5f, 0.5f * 7/6));
         Vector4f x = new Vector4f(x0);
         final float d = 0.01f;
         int i = 1000;
         do {
             Newton newton = new Newton(P, Q, Pn, Qn, x, d, i);
             x = newton.solve();
-            System.out.println(x);
-        } while(x.sub(x0, new Vector4f()).lengthSquared() > 0.0001f);
+        } while(x.sub(x0, new Vector4f()).lengthSquared() > 0.000001f);
     }
 }
