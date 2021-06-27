78a5b9d026cf5ac376b98cd3a6877a4a0c28dd0f
Tomasz Herman <tomasz.herman@protonmail.com>
2021-06-27 06:35:09 +0200

    Fixed intersection start when initial position was suggested

--- src/main/java/pl/edu/pw/mini/mg1/numerics/IntersectionStart.java
+++ src/main/java/pl/edu/pw/mini/mg1/numerics/IntersectionStart.java
@@ -6,10 +6,12 @@ import org.joml.Vector3f;
 import org.joml.Vector4f;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
 
 import static org.joml.Math.*;
 
@@ -31,24 +33,38 @@ public class IntersectionStart {
         this.self = self;
     }
 
+    private static double applyAsDouble(Pair<Pair<Vector2f, Vector3f>, Float> value) {
+        return (double) value.getRight();
+    }
+
     public Vector4f solve(Vector3f closeTo) {
         Pair<Vector2f, Vector3f> p = self ? Pair.of(new Vector2f(0.75f), P.apply(new Vector2f(0.75f))) : Pair.of(new Vector2f(0.5f), P.apply(new Vector2f(0.5f)));
         Pair<Vector2f, Vector3f> q = self ? Pair.of(new Vector2f(0.25f), P.apply(new Vector2f(0.25f))) : Pair.of(new Vector2f(0.5f), Q.apply(new Vector2f(0.5f)));
         float R = 1f;
         if(closeTo != null) {
-            List<Pair<Vector2f, Vector3f>> pTemp = new ArrayList<>();
-            List<Pair<Vector2f, Vector3f>> qTemp = new ArrayList<>();
-            for (int j = 0; j < 4096; j++) {
+            List<Pair<Pair<Vector2f, Vector3f>, Float>> pTemp = new ArrayList<>();
+            List<Pair<Pair<Vector2f, Vector3f>, Float>> qTemp = new ArrayList<>();
+            for (int j = 0; j < 65536; j++) {
                 Vector2f puv = uv(new Vector2f(rand(1), rand(1)).add(p.getLeft()));
                 Vector2f quv = uv(new Vector2f(rand(1), rand(1)).add(q.getLeft()));
                 Vector3f ppp = P.apply(puv);
                 Vector3f qqq = Q.apply(quv);
-                if(ppp.distanceSquared(closeTo) < 0.3) pTemp.add(Pair.of(puv, P.apply(puv)));
-                if(qqq.distanceSquared(closeTo) < 0.3) qTemp.add(Pair.of(quv, Q.apply(quv)));
+                pTemp.add(Pair.of(Pair.of(puv, ppp), ppp.distanceSquared(closeTo)));
+                qTemp.add(Pair.of(Pair.of(quv, qqq), qqq.distanceSquared(closeTo)));
             }
+            List<Pair<Vector2f, Vector3f>> ppTemp = pTemp.stream()
+                    .sorted(Comparator.comparingDouble(IntersectionStart::applyAsDouble).reversed())
+                    .limit(4096)
+                    .map(Pair::getLeft)
+                    .toList();
+            List<Pair<Vector2f, Vector3f>> qqTemp = qTemp.stream()
+                    .sorted(Comparator.comparingDouble(IntersectionStart::applyAsDouble).reversed())
+                    .limit(4096)
+                    .map(Pair::getLeft)
+                    .toList();
             float bestDist = p.getRight().distanceSquared(q.getRight());
-            for (Pair<Vector2f, Vector3f> qq : qTemp) {
-                for (Pair<Vector2f, Vector3f> pp : pTemp) {
+            for (Pair<Vector2f, Vector3f> qq : qqTemp) {
+                for (Pair<Vector2f, Vector3f> pp : ppTemp) {
                     if(self && pp.getLeft().distanceSquared(qq.getLeft()) < 0.3) continue;
                     float nextDist = qq.getRight().distanceSquared(pp.getRight());
                     if (nextDist < bestDist) {
@@ -63,26 +79,12 @@ public class IntersectionStart {
         for (int i = 0; i < 100; i++) {
             List<Pair<Vector2f, Vector3f>> pTemp = new ArrayList<>();
             List<Pair<Vector2f, Vector3f>> qTemp = new ArrayList<>();
-            for (int j = 0; j < (closeTo == null ? 1024 : 4096); j++) {
+            for (int j = 0; j < 1024; j++) {
                 Vector2f puv = uv(new Vector2f(rand(R), rand(R)).add(p.getLeft()));
                 Vector2f quv = uv(new Vector2f(rand(R), rand(R)).add(q.getLeft()));
                 pTemp.add(Pair.of(puv, P.apply(puv)));
                 qTemp.add(Pair.of(quv, Q.apply(quv)));
             }
-            if(closeTo != null) {
-                pTemp.sort((p1, p2) -> {
-                    float d1 = p1.getRight().distanceSquared(closeTo);
-                    float d2 = p2.getRight().distanceSquared(closeTo);
-                    return Float.compare(d1, d2);
-                });
-                qTemp.sort((p1, p2) -> {
-                    float d1 = p1.getRight().distanceSquared(closeTo);
-                    float d2 = p2.getRight().distanceSquared(closeTo);
-                    return Float.compare(d1, d2);
-                });
-            }
-            pTemp = pTemp.subList(0, 1024);
-            qTemp = qTemp.subList(0, 1024);
             float lastDist = p.getRight().distanceSquared(q.getRight());
             float bestDist = lastDist;
             for (Pair<Vector2f, Vector3f> qq : qTemp) {
