7b24739be5199dcc07cc8061668893691bbbf896
Tomasz Herman <tomasz.herman@protonmail.com>
2021-10-09 17:36:31 +0200

    Loading paths

---
+++ src/main/java/pl/edu/pw/mini/mg1/milling/Path.java
@@ -0,0 +1,84 @@
+package pl.edu.pw.mini.mg1.milling;
+
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Path {
+    private List<Vector3f> coords;
+
+    public Path(InputStream stream) throws IOException {
+        if(stream == null) throw new IllegalArgumentException("Got null stream.");
+        coords = new ArrayList<>();
+        Scanner scanner = new Scanner(stream);
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Map<Character, String> tokens = split(line);
+            Vector3f coord = new Vector3f();
+            if(tokens.containsKey('X')) {
+                try {
+                    coord.x = Float.parseFloat(tokens.get('X'));
+                } catch (NumberFormatException nfe) {
+                    throw new IOException(nfe);
+                }
+            } else {
+                if(!coords.isEmpty()) {
+                    coord.x = coords.get(coords.size() - 1).x;
+                }
+            }
+            if(tokens.containsKey('Y')) {
+                try {
+                    coord.y = Float.parseFloat(tokens.get('Y'));
+                } catch (NumberFormatException nfe) {
+                    throw new IOException(nfe);
+                }
+            } else {
+                if(!coords.isEmpty()) {
+                    coord.y = coords.get(coords.size() - 1).y;
+                }
+            }
+            if(tokens.containsKey('Z')) {
+                try {
+                    coord.z = Float.parseFloat(tokens.get('Z'));
+                } catch (NumberFormatException nfe) {
+                    throw new IOException(nfe);
+                }
+            } else {
+                if(!coords.isEmpty()) {
+                    coord.z = coords.get(coords.size() - 1).z;
+                }
+            }
+            coords.add(coord);
+        }
+        coords.forEach(System.out::println);
+    }
+
+    public List<Vector3fc> getCoords() {
+        return Collections.unmodifiableList(coords);
+    }
+
+    private static Map<Character, String> split(String line) {
+        Map<Character, String> map = new HashMap<>();
+        char code = '\0';
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+            char current = Character.toUpperCase(line.charAt(i));
+            if(Character.isAlphabetic(current)) {
+                if(temp.length() > 0) {
+                    map.put(code, temp.toString());
+                    temp = new StringBuilder();
+                }
+                code = current;
+            } else {
+                temp.append(current);
+            }
+        }
+        if(temp.length() > 0) map.put(code, temp.toString());
+        return map;
+    }
+}
---
+++ src/test/java/pl/edu/pw/mini/mg1/milling/PathTest.java
@@ -0,0 +1,44 @@
+package pl.edu.pw.mini.mg1.milling;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+class PathTest {
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(stream("/p1/t1.k16")),
+                Arguments.of(stream("/p1/t2.f12")),
+                Arguments.of(stream("/p1/t3.f12")),
+                Arguments.of(stream("/p1/t4.k16")),
+
+                Arguments.of(stream("/p2/1.k16")),
+                Arguments.of(stream("/p2/2.f12")),
+                Arguments.of(stream("/p2/3.f10")),
+                Arguments.of(stream("/p2/4.k08")),
+                Arguments.of(stream("/p2/5.k01"))
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void readFromFileTest(InputStream stream) {
+        try {
+            Path path = new Path(stream);
+            assertThat(path.getCoords()).isNotEmpty();
+        } catch (IOException e) {
+            fail("Shouldn't happen", e);
+        }
+    }
+
+    private static InputStream stream(String path) {
+        return PathTest.class.getResourceAsStream(path);
+    }
+}
